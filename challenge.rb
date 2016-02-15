74c6c28ef6e00e0f3f19ee8a5c6d263cdb0d6733
JenWeber <weberj10@gmail.com>
2016-02-15 14:16:28 -0500

    add box class

--- challenge.rb
+++ challenge.rb
@@ -1,3 +1,13 @@
+board = []
+
 def make_board (size)
-board = Array.new(size) {rand(2)}
+  board = Array.new(size) {rand(2)}
+end
+
+class Box
+  def initialize()
+    @live = false
+    @row = row
+    @col = col
+  end
 end
